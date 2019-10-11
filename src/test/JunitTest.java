59ee2cd88d0c90d17cfb4a2dff1b4c3bab5010cd
Max <max.dou6@gmail.com>
2019-10-11 17:25:46 -0600

    Updated JunitTest.java, added Junit test cases to test HighScore.java. Test cases are checkpreviousHighScore() , checkCompairHighScore(), checkWritingScore()

--- src/test/JunitTest.java
+++ src/test/JunitTest.java
@@ -3,6 +3,7 @@ package test;
 import org.junit.Assert;
 import org.junit.Test;
 
+import main.HighScore;
 import map.Collidable;
 import map.CollidableTester;
 import map.MapObject;
@@ -28,9 +29,35 @@ public class JunitTest {
 	}
 	
 	@Test
-	public void checkHighScore() {
+	public void checkpreviousHighScore() {
+		
+		int compairPrevHighScore = 218; //previous high score
+		HighScore checkHS = new HighScore();
+		int test = checkHS.previousHighScore();
+		Assert.assertEquals(compairPrevHighScore, test); // check the previous high score which is current set at 218, this test will work if it can successfully get the high score
+	}
+	
+	@Test
+	public void checkCompairHighScore() {
+		
+		boolean testFalse = false;
+		HighScore checkHSC = new HighScore();
+		boolean runFalseTest = checkHSC.compareScores(); //by default, both user score and the current high score (without fetching the previous high score) are set to zero
+														 //that being said, I expect it to be false
+		Assert.assertEquals(testFalse, runFalseTest);
 		
 	}
+	
+	//Comment (below) this test out when testing checkpreviousHighScore ///////////////
+	/*
+	@Test
+	public void checkWritingScore() {
+		
+		HighScore checkHSW = new HighScore();
+		checkHSW.writeInNewScore();		// this test will be successful IF it writes a zero into the HighScores.txt
+	}
+	*/
+	// Comment out (above) when not using /////////////////////////////////////////////
 	
 	@Test
 	public void checkKeys() {
